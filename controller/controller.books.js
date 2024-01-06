5964bb93fe84141638fc90775ed40318206bf294
polaris9017 <moonrise917@gmail.com>
2024-01-06 17:40:00 +0900

    도서 정보 API 컨트롤러 기능 일부 구현 (240106)
    카테고리별 및 신간 도서 조회 기능 구현

--- controller/controller.books.js
+++ controller/controller.books.js
@@ -3,11 +3,36 @@ const {StatusCodes} = require("http-status-codes");
 
 const getBooks = (req, res) => {
     const {catId, isNew} = req.query;
+    let values = [];
 
     let sql = 'SELECT * FROM books';
-    let sql_cat = 'SELECT * FROM books WHERE category_id = ?';
 
-    if (!(catId || isNew)) {
+    if (catId && isNew) {
+        sql += ' WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
+        values = [catId, isNew];
+    } else if (catId) {
+        sql += ' WHERE category_id = ?';
+        values = [catId];
+    } else if (isNew) {
+        sql += ' WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
+        values = [isNew];
+    }
+
+    if (values) {  // query 값이 존재할 경우
+        conn.query(sql, values, (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).send(err);
+            }
+            if (result[0])
+                return res.status(StatusCodes.OK).json(result);
+            else
+                return res.status(StatusCodes.NOT_FOUND).json({
+                    status: StatusCodes.NOT_FOUND,
+                    message: 'No books found matches the category.'
+                });
+        });
+    } else {  // query 값이 없으면 전체 조회
         conn.query(sql, (err, result) => {
             if (err) {
                 console.log(err);
@@ -15,15 +40,6 @@ const getBooks = (req, res) => {
             }
             return res.status(StatusCodes.OK).json(result);
         });
-    } else if (catId && isNew) {
-        conn.query(sql_cat, catId, (err, result) => {
-            if (err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).send(err);
-            }
-            return res.status(StatusCodes.OK).json(result);
-        });
-
     }
 };
 

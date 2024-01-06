72b1188b95ce366e424c62733fd9c2be89185e48
polaris9017 <moonrise917@gmail.com>
2024-01-06 16:42:42 +0900

    Update Bookstore.sql
    mysqldump로 dump, 차후 추가 및 수정될 수 있음 (240106)

--- sql/Bookstore.sql
+++ sql/Bookstore.sql
@@ -1,36 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19-11.2.2-MariaDB, for osx10.19 (arm64)
 --
--- Host: 172.17.0.2:3306
--- 생성 시간: 24-01-05 17:14
--- 서버 버전: 11.2.2-MariaDB-1:11.2.2+maria~ubu2204
--- PHP 버전: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: Bookstore
+-- ------------------------------------------------------
+-- Server version	11.2.2-MariaDB-1:11.2.2+maria~ubu2204
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- 데이터베이스: `Bookstore`
+-- Table structure for table `books`
 --
 
--- --------------------------------------------------------
-
---
--- 테이블 구조 `books`
---
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `books` (
   `id` int(11) NOT NULL,
   `title` varchar(50) NOT NULL,
-  `category_id` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
   `img_id` int(11) NOT NULL,
   `isbn` varchar(13) NOT NULL,
   `summary` varchar(1000) NOT NULL,
@@ -39,205 +33,184 @@ CREATE TABLE `books` (
   `pages` int(11) NOT NULL,
   `contents` text NOT NULL,
   `price` int(11) NOT NULL,
-  `pub_date` int(11) NOT NULL
+  `pub_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ref_books_category_id` (`category_id`),
+  CONSTRAINT `ref_books_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 테이블 구조 `carts`
+-- Dumping data for table `books`
 --
 
+
+--
+-- Table structure for table `carts`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `carts` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `book_id` int(11) NOT NULL,
-  `amount` int(11) NOT NULL
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `book_id` (`book_id`),
+  CONSTRAINT `ref_order_book_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 테이블 구조 `delivery`
+-- Dumping data for table `carts`
 --
 
-CREATE TABLE `delivery` (
+
+--
+-- Table structure for table `category`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
   `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+
+--
+-- Table structure for table `delivery`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `delivery` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `address` varchar(500) NOT NULL,
   `recipient` varchar(40) NOT NULL,
-  `phone` varchar(14) NOT NULL
+  `phone` varchar(14) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 테이블 구조 `likes`
+-- Dumping data for table `delivery`
 --
 
+
+--
+-- Table structure for table `likes`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `likes` (
   `user_id` varchar(100) NOT NULL,
-  `book_id` int(11) NOT NULL
+  `book_id` int(11) NOT NULL,
+  KEY `ref_likes_book_id` (`book_id`),
+  KEY `ref_like_user_id` (`user_id`),
+  CONSTRAINT `ref_like_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ref_likes_book_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- 테이블 구조 `orders`
---
-
-CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
-  `delivery_id` int(11) NOT NULL,
-  `total_price` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 테이블 구조 `order_list`
+-- Dumping data for table `likes`
 --
 
+
+--
+-- Table structure for table `order_list`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `order_list` (
   `order_id` int(11) NOT NULL,
-  `cart_id` int(11) NOT NULL
+  `cart_id` int(11) NOT NULL,
+  KEY `ref_order_list_id` (`order_id`),
+  KEY `ref_order_cart_id` (`cart_id`),
+  CONSTRAINT `ref_order_cart_id` FOREIGN KEY (`cart_id`) REFERENCES `carts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ref_order_list_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 테이블 구조 `users`
+-- Dumping data for table `order_list`
 --
 
+
+--
+-- Table structure for table `orders`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `delivery_id` int(11) NOT NULL,
+  `total_price` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ref_order_deliver_id` (`delivery_id`),
+  CONSTRAINT `ref_order_deliver_id` FOREIGN KEY (`delivery_id`) REFERENCES `delivery` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+
+--
+-- Table structure for table `users`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `user_id` varchar(100) NOT NULL,
-  `password` varchar(44) NOT NULL
+  `password` varchar(44) NOT NULL,
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 테이블 구조 `users_cred`
+-- Dumping data for table `users`
 --
 
+
+--
+-- Table structure for table `users_cred`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users_cred` (
   `user_id` varchar(100) NOT NULL,
-  `salt` varchar(16) NOT NULL
+  `salt` varchar(24) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `ref_user_id_cred` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 덤프된 테이블의 인덱스
+-- Dumping data for table `users_cred`
 --
 
---
--- 테이블의 인덱스 `books`
---
-ALTER TABLE `books`
-  ADD PRIMARY KEY (`id`);
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- 테이블의 인덱스 `carts`
---
-ALTER TABLE `carts`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `ref_order_book_id` (`book_id`);
-
---
--- 테이블의 인덱스 `delivery`
---
-ALTER TABLE `delivery`
-  ADD PRIMARY KEY (`id`);
-
---
--- 테이블의 인덱스 `likes`
---
-ALTER TABLE `likes`
-  ADD PRIMARY KEY (`user_id`),
-  ADD KEY `ref_like_book_id` (`book_id`);
-
---
--- 테이블의 인덱스 `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `ref_order_deliver_id` (`delivery_id`);
-
---
--- 테이블의 인덱스 `order_list`
---
-ALTER TABLE `order_list`
-  ADD KEY `ref_order_list_id` (`order_id`),
-  ADD KEY `ref_order_cart_id` (`cart_id`);
-
---
--- 테이블의 인덱스 `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- 테이블의 인덱스 `users_cred`
---
-ALTER TABLE `users_cred`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- 덤프된 테이블의 AUTO_INCREMENT
---
-
---
--- 테이블의 AUTO_INCREMENT `carts`
---
-ALTER TABLE `carts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- 테이블의 AUTO_INCREMENT `delivery`
---
-ALTER TABLE `delivery`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- 테이블의 AUTO_INCREMENT `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- 덤프된 테이블의 제약사항
---
-
---
--- 테이블의 제약사항 `carts`
---
-ALTER TABLE `carts`
-  ADD CONSTRAINT `ref_order_book_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`);
-
---
--- 테이블의 제약사항 `likes`
---
-ALTER TABLE `likes`
-  ADD CONSTRAINT `ref_like_book_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `ref_like_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- 테이블의 제약사항 `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `ref_order_deliver_id` FOREIGN KEY (`delivery_id`) REFERENCES `delivery` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
-
---
--- 테이블의 제약사항 `order_list`
---
-ALTER TABLE `order_list`
-  ADD CONSTRAINT `ref_order_cart_id` FOREIGN KEY (`cart_id`) REFERENCES `carts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `ref_order_list_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- 테이블의 제약사항 `users_cred`
---
-ALTER TABLE `users_cred`
-  ADD CONSTRAINT `ref_user_id_cred` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-06 16:22:27

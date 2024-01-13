7aa92a51de67b3a4540c38c3a12a09cd8d1d0c24
polaris9017 <moonrise917@gmail.com>
2024-01-13 14:52:16 +0900

    Revert "토큰 검증 로직 추가 (2401)"
    This reverts commit a0437e26190f98403de7d6603551317c768a2e24.

--- controller/controller.likes.js
+++ controller/controller.likes.js
@@ -1,72 +1,34 @@
-const jwt = require('jsonwebtoken');
 const conn = require('../database');
-const dotenv = require('dotenv');
 const {StatusCodes} = require("http-status-codes");
-
-dotenv.config();
 
 const addLike = (req, res) => {
     const {book_id} = req.params;
+    const {user_id} = req.body;
     let sql = 'INSERT INTO likes (user_id, book_id) VALUES (?, ?)';
-    let auth = verifyToken(req);
+    let values = [user_id, book_id];
 
-    if (auth instanceof jwt.TokenExpiredError) {
-        return res.status(StatusCodes.UNAUTHORIZED).json({
-            status: StatusCodes.UNAUTHORIZED,
-            message: 'Token expired'
-        });
-    } else {
-        let values = [auth['user_id'], book_id];
-
-        conn.query(sql, values, (err, result) => {
-            if (err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end({
-                    status: StatusCodes.BAD_REQUEST,
-                    message: 'Error adding like please try again.'
-                });
-            }
-            return res.status(StatusCodes.OK).json(result);
-        });
-    }
+    conn.query(sql, values, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end('Error adding like please try again.');
+        }
+        return res.status(StatusCodes.OK).json(result);
+    });
 };
 
 const deleteLike = (req, res) => {
     const {book_id} = req.params;
-
+    const {user_id} = req.body;
     let sql = 'DELETE FROM likes WHERE user_id = ? AND book_id = ?';
+    let values = [user_id, book_id];
 
-    let auth = verifyToken(req);
-
-    if (auth instanceof jwt.TokenExpiredError) {
-        return res.status(StatusCodes.UNAUTHORIZED).json({
-            status: StatusCodes.UNAUTHORIZED,
-            message: 'Token expired'
-        });
-    } else {
-        let values = [auth['user_id'], book_id];
-
-        conn.query(sql, values, (err, result) => {
-            if (err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end({
-                    status: StatusCodes.BAD_REQUEST,
-                    message: 'Error deleting like please try again.'
-                });
-            }
-            return res.status(StatusCodes.OK).json(result);
-        });
-    }
-};
-
-function verifyToken(req) {
-    try {
-        return jwt.verify(req.headers['authorization'], process.env.JWT_SECRET);
-    } catch (err) {
-        console.log('Exception occurred while verifying token');
-        console.log(err);
-        return err;
-    }
+    conn.query(sql, values, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end('Error deleting like please try again.');
+        }
+        return res.status(StatusCodes.OK).json(result);
+    });
 }
 
 module.exports = {

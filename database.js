751585ba7e65b50ba4fdb9aa5fd09057f23ef11e
polaris9017 <moonrise917@gmail.com>
2024-01-18 18:57:40 +0900

    mysql2 async 기능 추가

--- database.js
+++ database.js
@@ -1,6 +1,25 @@
-const mariadb = require('mysql2');
+const mariadb = require('mysql2/promise');
 
-const connection = mariadb.createConnection({
+class Connection {
+    constructor(config) {
+        this.connection = mariadb.createPool(config);
+    }
+
+    async query(sql, values) {
+        const conn = await this.connection.getConnection();
+        let results = [];
+        try {
+            results = await (values ? conn.query(sql, values) : conn.query(sql));
+        } catch (err) {
+            console.log('[Error] DB connection error: ' + err);
+        } finally {
+            conn.release();
+        }
+        return results;
+    }
+}
+
+const connection = new Connection({
     host: 'localhost',
     user: 'root',
     password: 'root',

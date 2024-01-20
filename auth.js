cdeeab10a4fe97e20b12dd035983ed6873039ffa
polaris9017 <moonrise917@gmail.com>
2024-01-21 04:26:41 +0900

    Update auth.js
    헤더에 토큰이 주어지지 않을 때 ReferenceError 예외 던짐 (240115)

--- auth.js
+++ auth.js
@@ -5,7 +5,8 @@ dotenv.config();
 
 const verifyToken = (req) => {
     try {
-        return jwt.verify(req.headers['authorization'], process.env.JWT_SECRET);
+        if (req.headers['authorization']) return jwt.verify(req.headers['authorization'], process.env.JWT_SECRET);
+        else throw new ReferenceError('No token provided');
     } catch (err) {
         console.log('Exception occurred while verifying token');
         console.log(err);
